7a2e0b2df97d8fb103b03c3336db5d3a4fb98eab
yiyu1314 <1339210095@qq.com>
2018-01-04 21:06:04 +0800

    良仓代码

--- sibu.js
+++ sibu.js
@@ -2,7 +2,7 @@
 //第一个参数 userJSON 传入JSON格式，
 //第二个参数 按钮
 //第三哥参数 指定是为登录还是注册  登录为login 注册为register
-function fun(json,callback)
+function server(json,cb)
 {
 		var xhr=new XMLHttpRequest();
 		xhr.onreadystatechange=function()
@@ -12,14 +12,15 @@ function fun(json,callback)
 				if(xhr.status>=200&&xhr.status<300||status==304)
 				{
 					var obj=JSON.parse(xhr.responseText);
-					callback(obj);
+					cb(obj);
 				}
 			}
 		}
 		
-		xhr.open("POST","http://csit.top/api_user.php",true);
+		xhr.open(json.leixing,json.url,true);
 		xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-	   	var jiehe="status="+json.status+"&"+zhuanhuan(json);
+		console.log(json.json);
+	   	var jiehe="status="+json.status+"&"+zhuanhuan(json.json);
 	   	xhr.send(jiehe);
 }
 //封装把json格式得转换成字符串
@@ -27,10 +28,8 @@ function zhuanhuan(usJSON){
 	var str="";
 	for(var k in usJSON)
 	{
-		console.log(usJSON[k]);
 		str+=k+"="+usJSON[k]+"&";
 	}
 	str=str.substring(0,str.length-1);
-	console.log(str);
 	return str;
 }
